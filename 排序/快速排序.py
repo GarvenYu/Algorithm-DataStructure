805bcea64a7040b64aec5c1cc9b7347faba269c7
ykbboom <1747603726@qq.com>
2018-07-19 22:26:33 +0800

    修改快速排序

--- 排序/快速排序.py
+++ 排序/快速排序.py
@@ -4,12 +4,14 @@ import random
 
 
 def swap(data, randIndex, end):
+	"""交换元素"""
     tmp = data[randIndex]
     data[randIndex] = data[end]
     data[end] = tmp
 
 
 def partition(data, start, end, length):
+	"""将某个元素放到合适位置并对数组进行分割(左小右大)"""
     if not data:
         return "Invalid Data"
     if not isinstance(start, int) or not isinstance(end, int) or not isinstance(length, int):
@@ -18,17 +20,18 @@ def partition(data, start, end, length):
         return "Error parameter"
     # 区间内随机取哨兵, start<=randIndex<=end
     randIndex = random.randint(start, end)
-    # 交换
+    # 交换 随机元素放到末尾
     swap(data, randIndex, end)
-    print(data)
-    print(str(start) + '->' + str(end))
     pivot = start - 1
     for index in range(start, end):
         if data[index] < data[end]:
             pivot += 1
             if pivot != index:
+            	# 把大元素换到后面
                 swap(data, pivot, index)
+    # 哨兵指向比随机元素大的元素
     pivot += 1
+    # 进行交换,此时 小 随机元素 大
     swap(data, pivot, end)
     return pivot
 
