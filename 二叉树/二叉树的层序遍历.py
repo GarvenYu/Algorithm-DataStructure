4500a9e12bdfc65cfd890c0e6c92fe7e7bee6a98
ykbboom <1747603726@qq.com>
2018-08-17 11:45:52 +0800

    mdf print

--- 二叉树/二叉树的层序遍历.py
+++ 二叉树/二叉树的层序遍历.py
@@ -5,8 +5,10 @@
 从上到下，同一层从左到右的顺序打印二叉树的每个节点。
 """
 
+
 class ArrayQueue(object):
 	"""ArrayQueue的实现"""
+
 	def __init__(self, arg):
 		pass
 
@@ -18,15 +20,18 @@ class ArrayQueue(object):
 		"""尾元素入队列"""
 		pass
 
+
 def print_node(root_node):
-	"""输出node的值"""
-	if not root_node.value:
-		return None
-	else:
-		print(root_node.value)
+    """输出node的值"""
+    if not root_node.value:
+        return None
+    else:
+        print(root_node.value, end='')
+
 
 def tree_level_traversal(queue, root_node):
-	if not queue or not root_node:
+    if not queue or not root_node:
+
 		return None
 	else:
 		# 根节点入队列
