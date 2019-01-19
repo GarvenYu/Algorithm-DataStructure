5542f97a74dfc549df62ed92e701971ae8fabf7c
GarvenYu <1747603726@qq.com>
2019-01-19 16:33:24 +0800

    找到链表倒数第k个节点

--- 链表/nth_node_from_last.py
+++ 链表/nth_node_from_last.py
@@ -6,38 +6,20 @@
 """
 
 
-class ListNode(object):
-
-    def __init__(self, value=None, node=None):
-        self.value = value
-        self.p_next = node
-
-
 def find_last_k_node(p_head, k):
     """找到倒数第K个节点"""
     if not p_head or k <= 0 or not isinstance(k, int):
         return None
     p_ahead = p_head
-    p_behind = None
-    while k - 1 > 0:
-        if p_ahead.p_next != None:
+    while k - 1:
+        if p_ahead.p_next:
             p_ahead = p_ahead.p_next
             k -= 1
         else:
             # K > 节点总数
             return None
     p_behind = p_head
-    while p_ahead.p_next != None:
+    while p_ahead.p_next:
         p_ahead = p_ahead.p_next
         p_behind = p_behind.p_next
     return p_behind
-
-if __name__ == '__main__':
-    node3 = ListNode(3, None)
-    node2 = ListNode(2, node3)
-    node1 = ListNode(1, node2)
-    node = find_last_k_node(node1, 4)
-    if node:
-        print(node.value)
-    else:
-        print(node)
