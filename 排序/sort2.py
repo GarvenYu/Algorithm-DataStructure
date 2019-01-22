12020f28461482b3430aba926ae6afac61fee850
GarvenYu <1747603726@qq.com>
2019-01-22 22:45:51 +0800

    快排和归并排序

--- 排序/sort2.py
+++ 排序/sort2.py
@@ -1,7 +1,9 @@
-#!usr/bin/env/ python
-#-*-coding:utf-8-*-
+#! usr/bin/env python3
+# -*-coding:utf-8-*-
 import random
 
+
+# 快速排序
 
 def swap(data, randIndex, end):
     """交换元素"""
@@ -44,6 +46,31 @@ def quickSort(data, start, end):
     return data
 
 
-if __name__ == '__main__':
-    data = [5, 3, 2, 7, 4, 1, 8, 9]
-    print(quickSort(data, 0, len(data) - 1))
+# 归并排序
+
+def merge_sort(alist):
+    if len(alist) <= 1:
+        return alist
+    # 二分分解
+    num = len(alist) // 2
+    left = merge_sort(alist[:num])
+    right = merge_sort(alist[num:])
+    # 合并
+    return merge(left, right)
+
+
+def merge(left, right):
+    """合并操作，将两个有序数组left[]和right[]合并成一个大的有序数组"""
+    # left与right的下标指针
+    l, r = 0, 0
+    result = []
+    while l < len(left) and r < len(right):
+        if left[l] < right[r]:
+            result.append(left[l])
+            l += 1
+        else:
+            result.append(right[r])
+            r += 1
+    result += left[l:]
+    result += right[r:]
+    return result
